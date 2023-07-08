4de01281fc2ee12fdf8a98c8cd981c8557679ea1
Mohammed AlSammak <msk170a@gmail.com>
2023-07-08 21:47:05 +0300

    added back-end and some endpoints

---
+++ server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"ConcertAPI/handlers"
+	"fmt"
+)
+
+func main() {
+	var r router
+	http.ListenAndServe(":8000", &r)
+}
+
+type router struct{}
+
+func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL.Path)
+    switch req.URL.Path {
+    case "/":
+    	ConcertAPI.Index(w, req)
+    case "/output.css": // Serves the css file
+    	ConcertAPI.StylesServ(w, req)
+    case "/static/vid.mp4":
+    	ConcertAPI.VidServe(w, req)
+
+    default:
+        http.Error(w, "404 Not Found", 404)
+    }
+}
