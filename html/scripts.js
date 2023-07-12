0063d35d4b03c75db35ca29cee74196dd4622856
M-SK <legendary512@gmail.com>
2023-07-13 02:24:34 +0300

    visual manipulation on html with dom

--- html/scripts.js
+++ html/scripts.js
@@ -1,28 +1,50 @@
-function BandInfo(bandid) {
-    let BandId = document.querySelector(".band_id"+bandid).id
-    console.log(BandId)
-    const get = async (url, params) => {
-        const response = await fetch(url, {
-            method: 'GET',
-            body: JSON.stringify(params),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
+var filtered_band_info
+
+async function BandInfo(bandid) {
+    /*let BandId = document.querySelector(".band_id"+bandid).id
+    console.log(BandId)*/
+
+    const url = '/bandinfo';
+    /*const data = {
+        username: 'johnDoe',
+        email: 'john@example.com'
+    };*/
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/text'
+        },
+        body: JSON.stringify(bandid)
+    })
+        .then(response => response.json())
+        .then(result => {
+            filtered_band_info = result
+            fetch_band_template(filtered_band_info)
+            // Handle the response data
         })
-    
-        const data = await response.json()
-    
-        return data
-    }
-    
-    // Then use it like so with async/await:
-    (async () => {
-        const response = await fetch('/artists?' + new URLSearchParams({
-            id: BandId
-        }))
-    
-        const data = await response.json()
-    
-        console.log(data)
-    })()
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle errors
+        });
+}
+
+function fetch_band_template(filtered_band_info) {
+    const url_arist_page = '/artisttemplate.html';
+    const targetElement = document.getElementsByTagName('body')[0];
+
+    fetch(url_arist_page)
+        .then(response => response.text())
+        .then(html => {
+            html = html.replace(searchValue = `.{{ImagePath}}`, replaceValue = filtered_band_info.image)
+            html = html.replace(searchValue = `.{{ArtistName}}`, replaceValue = filtered_band_info.name)
+            html = html.replace(searchValue = `.{{ArtistMembers}}`, replaceValue = filtered_band_info.members)
+            html = html.replace(searchValue = `.{{CreationDate}}`, replaceValue = filtered_band_info.creationData)
+            html = html.replace(searchValue = `.{{FirstAlbumDate}}`, replaceValue = filtered_band_info.firstAlbum)
+            targetElement.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Handle errors
+        });
 }
