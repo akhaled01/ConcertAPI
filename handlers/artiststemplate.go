7c5bcdf8175e9ac80ed4ae2a37fda62842b986f5
Mohammed AlSammak <msk170a@gmail.com>
2023-07-12 23:42:57 +0300

    added struct for json parsing

--- handlers/artiststemplate.go
+++ handlers/artiststemplate.go
@@ -1,8 +1,19 @@
 package ConcertAPI
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+type Artist struct {
+	id           int      `json:"id"`
+	image        string   `json:"image"`
+	members      []string `json:"members"`
+	creationData int      `json:"creationData"`
+	firstAlbum   string   `json:"firstAlbum"`
+	locations    string   `json:"locations"`
+	relations    string   `json:"relations"`
+}
 
 func ArtistTemplate(w http.ResponseWriter, req *http.Request) {
 	origin := req.Header.Get("Origin")
